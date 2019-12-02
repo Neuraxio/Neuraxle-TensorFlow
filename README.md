bb20f6b664f574e48db415ea5493b4cef79853c5
alexbrillant <abrillant23@gmail.com>
2019-12-02 17:38:23 -0500

    Create Setup.py, And Folder Structure

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Neuraxle-TensorFlow
-TensorFlow steps, savers, and utilities for Neuraxle.
+
+TensorFlow steps, savers, and utilities for [Neuraxle](https://github.com/Neuraxio/Neuraxle).
+
+Neuraxle is a Machine Learning (ML) library for building neat pipelines, providing the right abstractions to both ease research, development, and deployment of your ML applications.
+
+## Installation for tensorflow>=1.15
+
+```
+neuraxle-tensorflow[tf]
+```
+
+## Installation for tensorflow-gpu>=1.15
+
+```
+neuraxle-tensorflow[tf_gpu]
+```
+
+## Usage example
+
+```python
+class YourTensorflowModelWrapper(BaseStep):
+    def __init__(self):
+        BaseStep.__init__(
+            self, 
+            hyperparams=HYPERPARAMS,
+            savers=[TensorflowV1StepSaver()]
+        )
+```
+
+[See also a complete example](https://github.com/Neuraxio/LSTM-Human-Activity-Recognition/blob/neuraxle-refactor/steps/lstm_rnn_tensorflow_model_wrapper.py)
---
+++ neuraxle_tensorflow/tensorflow_v1.py
@@ -0,0 +1,97 @@
+"""
+Neuraxle Tensorflow V1 Utility classes
+====================================
+Neuraxle utility classes for tensorflow v1.
+..
+    Copyright 2019, Neuraxio Inc.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+import os
+import tensorflow as tf
+
+from neuraxle.base import BaseSaver
+
+
+class TensorflowV1StepSaver(BaseSaver):
+    """
+    Step saver for a tensorflow Session using tf.train.Saver().
+    It saves, or restores the tf.Session() checkpoint at the context path using the step name as file name.
+    .. seealso::
+        `Using the saved model format <https://www.tensorflow.org/guide/saved_model>`_
+    """
+
+    def save_step(self, step: 'BaseStep', context: 'ExecutionContext') -> 'BaseStep':
+        """
+        Save a step that is using tf.train.Saver().
+        :param step: step to save
+        :type step: BaseStep
+        :param context: execution context to save from
+        :type context: ExecutionContext
+        :return: saved step
+        """
+        with step.get_graph().as_default():
+            saver = tf.train.Saver()
+            saver.save(
+                step.get_session(),
+                self._get_saved_model_path(context, step)
+            )
+
+            step.strip()
+
+        return step
+
+    def load_step(self, step: 'BaseStep', context: 'ExecutionContext') -> 'BaseStep':
+        """
+        Load a step that is using tensorflow using tf.train.Saver().
+        :param step: step to load
+        :type step: BaseStep
+        :param context: execution context to load from
+        :type context: ExecutionContext
+        :return: loaded step
+        """
+        step.is_initialized = False
+        step.setup()
+        with step.get_graph().as_default():
+            saver = tf.train.Saver()
+            saver.restore(
+                step.get_session(),
+                self._get_saved_model_path(context, step)
+            )
+
+        return step
+
+    def can_load(self, step: 'BaseStep', context: 'ExecutionContext'):
+        """
+        Returns whether or not we can load.
+        :param step: step to load
+        :type step: BaseStep
+        :param context: execution context to load from
+        :type context: ExecutionContext
+        :return: loaded step
+        """
+        meta_exists = os.path.exists(os.path.join(context.get_path(), "{0}.ckpt.meta".format(step.get_name())))
+        index_exists = os.path.exists(os.path.join(context.get_path(), "{0}.ckpt.index".format(step.get_name())))
+        return meta_exists and index_exists
+
+    def _get_saved_model_path(self, context, step):
+        """
+        Returns the saved model path using the given execution context, and step name.
+        :param step: step to load
+        :type step: BaseStep
+        :param context: execution context to load from
+        :type context: ExecutionContext
+        :return: loaded step
+        """
+        return os.path.join(
+            context.get_path(),
+            "{0}.ckpt".format(step.get_name())
+        )
