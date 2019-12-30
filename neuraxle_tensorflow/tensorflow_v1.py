8337d090af7bf1c97f9a1bc2e89d8abac00448f4
alexbrillant <abrillant23@gmail.com>
2019-12-30 18:05:15 -0500

    Simplify Tensorflow 1 Model And Saver Wip

--- neuraxle_tensorflow/tensorflow_v1.py
+++ neuraxle_tensorflow/tensorflow_v1.py
@@ -20,33 +20,77 @@ import os
 from abc import abstractmethod
 
 import tensorflow as tf
+from neuraxle.base import BaseSaver, BaseStep, ExecutionContext
 
-from neuraxle.base import BaseSaver
 
+class BaseTensorflowV1ModelStep(BaseStep):
+    def __init__(
+            self,
+            variable_scope=None,
+            hyperparams=None
+    ):
+        BaseStep.__init__(
+            self,
+            savers=[TensorflowV1StepSaver()],
+            hyperparams=hyperparams
+        )
 
-class TensorflowV1ModelWrapperMixin:
-    """
-    A class that represents a step that contains a tensorflow v1 model.
+        self.variable_scope = variable_scope
+        self.tensorflow_props = {}
 
-    .. seealso::
-        `Using the saved model format <https://www.tensorflow.org/guide/saved_model>`_
-    """
-    @abstractmethod
-    def get_session(self):
-        pass
+    def setup(self) -> BaseStep:
+        if self.is_initialized:
+            return self
 
-    @abstractmethod
-    def get_graph(self):
-        pass
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            with tf.variable_scope(self.variable_scope, reuse=tf.AUTO_REUSE):
+                self.session = tf.Session(config=tf.ConfigProto(log_device_placement=True), graph=self.graph)
 
-    @abstractmethod
+                self.tensorflow_props = self.setup_graph()
+                self.tensorflow_props = self.tensorflow_props if self.tensorflow_props is not None else {}
+
+                init = tf.global_variables_initializer()
+                self.session.run(init)
+                self.is_initialized = True
+
+    def teardown(self):
+        if self.session is not None:
+            self.session.close()
+        self.is_initialized = False
+
     def strip(self):
-        """
-        Get the tensorflow tf.Graph() object.
+        self.tensorflow_props = {}
+        self.graph = None
+        self.session = None
 
-        :return: tf.Graph
-        """
+    @abstractmethod
+    def setup_graph(self):
         raise NotImplementedError()
+
+    def fit(self, data_inputs, expected_outputs=None) -> 'BaseStep':
+        with tf.variable_scope(self.variable_scope, reuse=tf.AUTO_REUSE):
+            return self.fit_model(data_inputs, expected_outputs)
+
+    @abstractmethod
+    def fit_model(self, data_inputs, expected_outputs=None) -> BaseStep:
+        raise NotImplementedError()
+
+    def transform(self, data_inputs, expected_outputs=None) -> 'BaseStep':
+        with tf.variable_scope(self.variable_scope, reuse=tf.AUTO_REUSE):
+            return self.transform_model(data_inputs)
+
+    @abstractmethod
+    def transform_model(self, data_inputs):
+        raise NotImplementedError()
+
+    def __getitem__(self, item):
+        if item in self.tensorflow_props:
+            return self.tensorflow_props[item]
+
+        return self.graph.get_tensor_by_name(
+            "{0}/{1}:0".format(self.variable_scope, item)
+        )
 
 
 class TensorflowV1StepSaver(BaseSaver):
@@ -57,7 +101,7 @@ class TensorflowV1StepSaver(BaseSaver):
         `Using the saved model format <https://www.tensorflow.org/guide/saved_model>`_
     """
 
-    def save_step(self, step: 'TensorflowV1ModelWrapperMixin', context: 'ExecutionContext') -> 'BaseStep':
+    def save_step(self, step: 'BaseTensorflowV1ModelStep', context: 'ExecutionContext') -> 'BaseStep':
         """
         Save a step that is using tf.train.Saver().
         :param step: step to save
@@ -66,18 +110,14 @@ class TensorflowV1StepSaver(BaseSaver):
         :type context: ExecutionContext
         :return: saved step
         """
-        with step.get_graph().as_default():
+        with step.graph.as_default():
             saver = tf.train.Saver()
-            saver.save(
-                step.get_session(),
-                self._get_saved_model_path(context, step)
-            )
-
+            saver.save(step.session, self._get_saved_model_path(context, step))
             step.strip()
 
         return step
 
-    def load_step(self, step: 'TensorflowV1ModelWrapperMixin', context: 'ExecutionContext') -> 'BaseStep':
+    def load_step(self, step: 'BaseTensorflowV1ModelStep', context: 'ExecutionContext') -> 'BaseStep':
         """
         Load a step that is using tensorflow using tf.train.Saver().
         :param step: step to load
@@ -88,16 +128,14 @@ class TensorflowV1StepSaver(BaseSaver):
         """
         step.is_initialized = False
         step.setup()
-        with step.get_graph().as_default():
+
+        with step.graph.as_default():
             saver = tf.train.Saver()
-            saver.restore(
-                step.get_session(),
-                self._get_saved_model_path(context, step)
-            )
+            saver.restore(step.session, self._get_saved_model_path(context, step))
 
         return step
 
-    def can_load(self, step: 'TensorflowV1ModelWrapperMixin', context: 'ExecutionContext'):
+    def can_load(self, step: 'BaseTensorflowV1ModelStep', context: 'ExecutionContext'):
         """
         Returns whether or not we can load.
         :param step: step to load
@@ -108,9 +146,10 @@ class TensorflowV1StepSaver(BaseSaver):
         """
         meta_exists = os.path.exists(os.path.join(context.get_path(), "{0}.ckpt.meta".format(step.get_name())))
         index_exists = os.path.exists(os.path.join(context.get_path(), "{0}.ckpt.index".format(step.get_name())))
+
         return meta_exists and index_exists
 
-    def _get_saved_model_path(self, context, step):
+    def _get_saved_model_path(self, context: ExecutionContext, step: BaseStep):
         """
         Returns the saved model path using the given execution context, and step name.
         :param step: step to load
@@ -119,7 +158,4 @@ class TensorflowV1StepSaver(BaseSaver):
         :type context: ExecutionContext
         :return: loaded step
         """
-        return os.path.join(
-            context.get_path(),
-            "{0}.ckpt".format(step.get_name())
-        )
+        return os.path.join(context.get_path(), "{0}.ckpt".format(step.get_name()))
--- testing/test_tensorflow_v1.py
+++ testing/test_tensorflow_v1.py
@@ -1,90 +1,153 @@
+from typing import Dict
+
+import numpy as np
 import tensorflow as tf
 from neuraxle.base import BaseStep, ExecutionContext
+from neuraxle.hyperparams.space import HyperparameterSamples
 from neuraxle.pipeline import Pipeline
 
-from neuraxle_tensorflow.tensorflow_v1 import TensorflowV1StepSaver, TensorflowV1ModelWrapperMixin
+from neuraxle_tensorflow.tensorflow_v1 import BaseTensorflowV1ModelStep
 
 MATMUL_VARIABLE_SCOPE = "matmul"
 
 
-class TensorflowMatMulModel(TensorflowV1ModelWrapperMixin, BaseStep):
-    def __init__(self):
-        BaseStep.__init__(self, savers=[TensorflowV1StepSaver()])
+class Tensorflow1Model(BaseTensorflowV1ModelStep):
+    def __init__(self, variable_scope=None):
+        if variable_scope is None:
+            variable_scope = 'Tensorflow1Model'
+        BaseTensorflowV1ModelStep.__init__(
+            self,
+            variable_scope=variable_scope,
+            hyperparams=HyperparameterSamples({
+                'learning_rate': 0.01
+            })
+        )
 
-        self.graph = None
-        self.sess = None
+    def setup_graph(self) -> Dict:
+        forward(self.hyperparams)
+        tf.equal(tf.argmax(self['pred'], 1), tf.argmax(self['y'], 1), name='correct_pred')
+        tf.reduce_mean(tf.cast(self['correct_pred'], tf.float32), name='accuracy')
 
-    def setup(self) -> BaseStep:
-        if self.is_initialized:
-            return self
+        l2 = self.hyperparams['lambda_loss_amount'] * sum(tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables())
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self['y'], logits=self['pred'])) + l2
 
-        self.graph = tf.Graph()
+        optimizer = tf.train.AdamOptimizer(
+            learning_rate=self.hyperparams['learning_rate']
+        ).minimize(loss)
 
-        with self.graph.as_default():
-            self.in_a = tf.placeholder(dtype=tf.float32, shape=2)
-            self.in_b = tf.placeholder(dtype=tf.float32, shape=2)
+        return {
+            'loss': loss,
+            'optimizer': optimizer
+        }
 
-            self.out_a = forward(self.in_a)
-            self.out_b = forward(self.in_b)
-            self.reg_loss = tf.losses.get_regularization_loss(scope=MATMUL_VARIABLE_SCOPE)
+    def fit_model(self, data_inputs, expected_outputs=None) -> BaseStep:
+        if not isinstance(data_inputs, np.ndarray):
+            data_inputs = np.array(data_inputs)
 
-            with tf.variable_scope(MATMUL_VARIABLE_SCOPE, reuse=tf.AUTO_REUSE):
-                self.create_session()
-                self.is_initialized = True
+        if not isinstance(expected_outputs, np.ndarray):
+            expected_outputs = np.array(expected_outputs)
+
+        if expected_outputs.shape != (len(data_inputs), self.hyperparams['n_classes']):
+            expected_outputs = np.reshape(expected_outputs, (len(data_inputs), self.hyperparams['n_classes']))
+
+            _, loss, acc = self.session.run(
+                [self['optimizer'], self['loss'], self['accuracy']],
+                feed_dict={
+                    self['x']: data_inputs,
+                    self['y']: expected_outputs
+                }
+            )
+
+            print("Batch Loss = " + "{:.6f}".format(loss) + ", Accuracy = {}".format(acc))
+
+        self.is_invalidated = True
 
         return self
 
-    def strip(self):
-        self.sess = None
-        self.graph = None
-        self.in_a = None
-        self.in_b = None
-        self.out_a = None
-        self.out_b = None
-        self.reg_loss = None
+    def transform_model(self, data_inputs):
+        if not isinstance(data_inputs, np.ndarray):
+            data_inputs = np.array(data_inputs)
 
-    def create_session(self):
-        self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True), graph=self.graph)
-        init = tf.global_variables_initializer()
-        self.sess.run(init)
-
-    def get_session(self):
-        return self.sess
-
-    def get_graph(self):
-        return self.graph
-
-    def teardown(self):
-        if self.sess is not None:
-            self.sess.close()
-        self.is_initialized = False
-
-    def fit(self, data_inputs, expected_outputs=None) -> 'BaseStep':
-        results = self.sess.run([self.out_a, self.out_b, self.reg_loss],
-                                feed_dict={self.in_a: data_inputs[0], self.in_b: data_inputs[1]})
-        loss = results[2]
-        return self
-
-    def transform(self, data_inputs):
-        return self.sess.run([self.out_a, self.out_b],
-                             feed_dict={self.in_a: data_inputs[0], self.in_b: data_inputs[1]})
+        return self.session.run(
+            [self['pred']],
+            feed_dict={self['x']: data_inputs}
+        )[0]
 
 
-def forward(x):
-    with tf.variable_scope(MATMUL_VARIABLE_SCOPE, reuse=tf.AUTO_REUSE):
-        W = tf.get_variable("W", initializer=tf.ones(shape=(2, 2)),
-                            regularizer=tf.contrib.layers.l2_regularizer(0.04))
-        b = tf.get_variable("b", initializer=tf.zeros(shape=2))
-        return W * x + b
+def forward(hyperparams):
+    # Function returns a tensorflow LSTM (RNN) artificial neural network from given parameters.
+    # Moreover, two LSTM cells are stacked which adds deepness to the neural network.
+    # Note, some code of this notebook is inspired from an slightly different
+    # RNN architecture used on another dataset, some of the credits goes to
+    # "aymericdamien" under the MIT license.
+    # (NOTE: This step could be greatly optimised by shaping the dataset once
+    # input shape: (batch_size, n_steps, n_input)
+
+    # Graph input/output
+    x = tf.placeholder(tf.float32, [None, hyperparams['n_steps'], hyperparams['n_inputs']], name='x')
+    y = tf.placeholder(tf.float32, [None, hyperparams['n_classes']], name='y')
+
+    # Graph weights
+    weights = {
+        'hidden': tf.Variable(
+            tf.random_normal([hyperparams['n_inputs'], hyperparams['n_hidden']])
+        ),  # Hidden layer weights
+        'out': tf.Variable(
+            tf.random_normal([hyperparams['n_hidden'], hyperparams['n_classes']], mean=1.0)
+        )
+    }
+
+    biases = {
+        'hidden': tf.Variable(
+            tf.random_normal([hyperparams['n_hidden']])
+        ),
+        'out': tf.Variable(
+            tf.random_normal([hyperparams['n_classes']])
+        )
+    }
+
+    data_inputs = tf.transpose(
+        x,
+        [1, 0, 2])  # permute n_steps and batch_size
+
+    # Reshape to prepare input to hidden activation
+    data_inputs = tf.reshape(data_inputs, [-1, hyperparams['n_inputs']])
+    # new shape: (n_steps*batch_size, n_input)
+
+    # ReLU activation, thanks to Yu Zhao for adding this improvement here:
+    _X = tf.nn.relu(
+        tf.matmul(data_inputs, weights['hidden']) + biases['hidden']
+    )
+
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(_X, hyperparams['n_steps'], 0)
+    # new shape: n_steps * (batch_size, n_hidden)
+
+    # Define two stacked LSTM cells (two recurrent layers deep) with tensorflow
+    lstm_cell_1 = tf.contrib.rnn.BasicLSTMCell(hyperparams['n_hidden'], forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(hyperparams['n_hidden'], forget_bias=1.0, state_is_tuple=True)
+    lstm_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
+
+    # Get LSTM cell output
+    outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _X, dtype=tf.float32)
+
+    # Get last time step's output feature for a "many-to-one" style classifier,
+    # as in the image describing RNNs at the top of this page
+    lstm_last_output = outputs[-1]
+
+    # Linear activation
+    pred = tf.matmul(lstm_last_output, weights['out']) + biases['out']
+    return tf.identity(pred, name='pred')
 
 
 def test_tensorflowv1_saver(tmpdir):
-    model = Pipeline([TensorflowMatMulModel()])
+    model = Pipeline([
+        Tensorflow1Model()
+    ])
 
-    model = model.fit([[2, 1], [2, 4]])
     model.save(ExecutionContext(root=tmpdir))
 
-    loaded = Pipeline([TensorflowMatMulModel()]).load(ExecutionContext(root=tmpdir))
+    loaded = Pipeline([Tensorflow1Model()]).load(ExecutionContext(root=tmpdir))
     outputs = loaded.transform([[3, 1], [5, 4]])
 
     assert outputs
